1a0cee9f9df803c819d5bb9ee62a0bf291d94219
Nastya <ael24@tpu.ru>
2025-09-06 02:40:46 +0700

    Первый коммит

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include "lib.h"
+
+#include <iostream>
+
+int main(int, char **)
+{
+    std::cout << "Hello, world! ";
+    std::cout << "Version 1.0." << version() << std::endl;
+    return 0;
+}
